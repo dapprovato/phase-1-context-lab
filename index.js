f6cee9fe67d6f9f3681da949dd1d2786735710d7
Daria Approvato <dariaapprovato@gmail.com>
2022-09-18 22:28:09 -0400

    commit tree cc25af3edc9e8e5598893977a1b95ab07874aaf8

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
-/* Your Code Here */
+function createEmployeeRecord(record) {
+    return {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(records) {
+    return records.map(function (record) {
+        return createEmployeeRecord(record)
+    })
+}
+
+function createTimeInEvent(dateStamp) {
+    let dateAndTime = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.substring(11)),
+        date: dateStamp.substring(0, 10)
+    }
+    let updatedTimeIn = [dateAndTime, ...this.timeInEvents]
+    this.timeInEvents = updatedTimeIn
+    return this
+}
+
+
+function createTimeOutEvent(dateStamp) {
+    let dateAndTime = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.substring(11)),
+        date: dateStamp.substring(0, 10)
+    }
+    let updatedTimeOut = [dateAndTime, ...this.timeOutEvents]
+    this.timeOutEvents = updatedTimeOut
+    return this
+}
+
+function hoursWorkedOnDate(dateWorked) {
+    let timeIn = this.timeInEvents.find(function (timeInEvent) {
+        return timeInEvent.date === dateWorked
+    })
+    let timeOut = this.timeOutEvents.find(function (timeOutEvent) {
+        return timeOutEvent.date === dateWorked
+    })
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(dateWorked) {
+    let payOwed = this.payPerHour
+    return payOwed * hoursWorkedOnDate.call(this, dateWorked)
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    let employeeName = srcArray.find(function (employee) {
+        return (firstName === employee.firstName)
+    })
+    return employeeName
+}
+
+function calculatePayroll(employeeRecords) {
+    const payroll = employeeRecords.reduce(function (memo, record){
+        return memo + allWagesFor.call(record)
+    }, 0)
+    return payroll
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +87,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
